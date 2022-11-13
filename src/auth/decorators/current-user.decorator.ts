ebc78982060db39f757fb6cb02a5c16f4ce068a2
Hani Ghazi <hani@tryadvocate.com>
2022-11-13 20:23:49 +0400

    [Add]: current user decorators

---
+++ src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+import { UserContext } from '@types';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext<UserContext>().req.user;
+  },
+);
