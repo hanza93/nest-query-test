cc7d21f735d66525937b10d0000f150475117fb1
Hani Ghazi <hani@tryadvocate.com>
2022-11-13 20:24:25 +0400

    [Add]: user errors.

---
+++ shared/errors/index.ts
@@ -0,0 +1,2 @@
+export * from './users.errors';
+export * from './parts.errors';
---
+++ shared/errors/users.errors.ts
@@ -0,0 +1,26 @@
+export const UsersErrors = {
+  invalidCredentials: {
+    code: 'invalid_credentials',
+    message: 'Invalid Credentials',
+  },
+  emailAlreadyExist: {
+    code: 'email_already_exits',
+    message: 'Email Already exists',
+  },
+  invalidCode: {
+    code: 'invalid_code',
+    message: 'Invalid Code',
+  },
+  notFound: {
+    code: 'user_not_found',
+    message: 'User not found',
+  },
+  oldPasswordMissMatch: {
+    code: 'old_password_miss_match',
+    message: 'Old password miss match',
+  },
+  unauthorized: {
+    code: 'user_unauthorized',
+    message: 'User unauthorized',
+  },
+};
